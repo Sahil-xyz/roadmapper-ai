c8945e2ed8c427688f6593b3fac6f748f67bba28
Sahilxyz <sahilxyz505@gmail.com>
2025-11-03 18:56:17 +0530

    Added new pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,14 @@
-export default function AboutPage() {
+// app/roadmap/page.tsx
+import { Header } from "@/components/Header";
+import RoadmapListPage from "@/components/RoadmapListPage";
+import  AboutDetails from '@/components/AboutDetails'
+
+export default function RoadmapPage() {
   return (
     <>
-      <main className="max-w-3xl mx-auto py-16 px-6 space-y-6 text-center">
-        <h1 className="text-3xl font-bold text-blue-600">About Roadmapper AI</h1>
-        <p className="text-gray-700">
-          Roadmapper AI was built to help learners plan and track their growth journey in any field.
-          Our mission is to simplify skill development with personalized, AI-generated learning paths.
-        </p>
-        <p className="text-gray-700">
-          Powered by Gemini AI, Next.js, Prisma, and Clerk Authentication.
-        </p>
+      <main className="min-h-screen p-6">
+        <Header/>
+        <AboutDetails />
       </main>
     </>
   );
---
+++ components/AboutDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutDetails () {
+  return (
+    <div className='flex items-center justify-center font-bold text-3xl h-screen w-screen'>
+        <p>About section is yet to build...</p>
+    </div>
+  )
+}
